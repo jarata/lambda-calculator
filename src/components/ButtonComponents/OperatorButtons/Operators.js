364e2925576774890c4c243d7de3591cb1827b25
Justin A <jarata@gmail.com>
2019-12-11 12:52:44 -0800

    mapping over data for setting up props: Operators.js, OperatorButton.js

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 import { operators } from "../../../data";
+import OperatorButton from "./OperatorButton";
 //Import your array data to from the provided data file
 
 const Operators = () => {
@@ -12,6 +13,9 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operatorState.map((op, index) => <OperatorButton key={index} op={op}/>)}
     </div>
   );
 };
+
+export default Operators;
