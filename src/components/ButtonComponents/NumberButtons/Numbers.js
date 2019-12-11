c0cb264a312ae8ff296b065454f1a0b38d4b02de
Justin A <jarata@gmail.com>
2019-12-11 12:45:15 -0800

    mapping over data for setting up props: Numbers.js, NumberButton.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -7,6 +7,7 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 import { numbers } from "../../../data";
+import NumberButton from "./NumberButton";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
@@ -16,6 +17,9 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numberState.map((num, index) => <NumberButton key={index} num={num}/>)}
     </div>
   );
 };
+
+export default Numbers;
